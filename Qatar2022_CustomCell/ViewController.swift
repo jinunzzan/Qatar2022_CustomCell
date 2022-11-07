b22451d8d8076e2bc5eefd49b002142135b8bcde
jinunzzan <lovebox6607@gmail.com>
2022-11-08 01:10:38 +0900

    customcell로 테이블뷰 구성하기

--- Qatar2022_CustomCell/ViewController.swift
+++ Qatar2022_CustomCell/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.rowHeight = 100
+        
     }
 
 
+}
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return nations.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        let keys = nations.keys.sorted()
+        let key = keys[section]
+        
+        if let group = nations[key]{
+            return group.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "nationcell", for: indexPath)
+        let keys = nations.keys.sorted()
+        let key = keys[indexPath.section]
+        
+        guard let group = nations[key] else {fatalError()}
+        let nation = group[indexPath.row]
+        
+        let imageflag = cell.viewWithTag(1) as? UIImageView
+        imageflag?.image = UIImage(named: nation.flag)
+        
+        let lblName = cell.viewWithTag(2) as? UILabel
+        lblName?.text = nation.name
+        
+        let lblGroup = cell.viewWithTag(3) as? UILabel
+        lblGroup?.text = "Group \(nation.group)"
+        
+        return cell
+        
+    }
+    
+    
 }
 
